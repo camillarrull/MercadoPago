c13d18fd70770bc0ddbcd2899f8f6b8f50e58e01
Franco Romaniello <romax.cs@gmail.com>
2021-07-23 08:03:00 -0300

    Arreglado código
    FIX las querys.
    FIX la base de datos y agregado .sql para poder importarla.
    Agregado dotenv para archivo .env.
    FIX Seguridad: Obtener el precio de los productos desde el back-end no dejarlo en los inputs del form, esto puede generar que los clientes editen el precio.
    TODO: Armar el endpoint del webhook.

--- src/index.js
+++ src/index.js
@@ -5,51 +5,122 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
-const https = require('https');
-//const { Connection } = require('./configs/db');
-//const router = require('./routes/routes');
-//const swaggerUi = require('swagger-ui-express'); 
-//const swaggerDocument = require('../swagger.json'); 
+const https = require('http');
+const mysql = require('mysql');
+const mercadopago = require('mercadopago'); 
+require('dotenv').config();
  
-//require('dotenv').config();
- 
-const host = '65.21.115.133';
-const port = 2053;
- 
+/*
+https/ssl
+
 const key	= readFileSync('/etc/letsencrypt/live/francoromaniello.com-0001/privkey.pem', 'utf8');
 const cert = readFileSync('/etc/letsencrypt/live/francoromaniello.com-0001/fullchain.pem', 'utf8');
- /*
+*/
+
+mercadopago.configure({
+    access_token: process.env.MERCADOPAGO_TOKEN
+});
+
+let connection = null;
+
 const onConnect = () => {
 	return new Promise((resolve, reject)=> {
-	const { connection } = new Connection();
-	connection.authenticate()
-		.then( () => resolve('CONECTADO A DB') )
-		.catch( (error) => reject(`Error ${error}`) );
+		connection = mysql.createConnection({
+			host: process.env.MYSQL_HOST,
+			user: process.env.MYSQL_USER,
+			password: process.env.MYSQL_PASSWORD,
+			database: process.env.MYSQL_DB,
+		});
+		
+		connection.connect(function(error) {
+			if(error) {
+				console.log('[MySql Service] Error al conectar con el servidor.');
+				reject('[MySql Service] Error al conectar con el servidor.');
+				return;
+			}
+
+			console.log('[MySql Service] Conectado.');
+			resolve('[MySql Service] Conectado.');
+		})
 	});
 }
  
 onConnect()
-	.then(async () => {*/
+	.then(async () => {
 	app.use(cors());
-	//app.use(express.static(path.join(__dirname, 'public/cs-dashboard')));
 	app.use(express.json());
-	//app.use('/', router);
- 
-	app.get('*', (req, res) => { 
-		res.sendFile(path.join(__dirname, '../public/index.html'));
-	});
-	
+	app.use(express.urlencoded({ extended: true }));
+
 	app.post('/webhook/payment', (req, res) => {
 		console.log(req.body);
 		res.status(200).send('ok');
 	});
 	
-	app.get('/', (req, res) => {
+	// Esta ruta es llamada por tu formulario previo a mandarlo a MercadoPago
+	app.post('/checkout', (req, res) => {
+		console.log('SELECT * FROM items WHERE id="'+parseInt(req.body.item_id)+'"');
 		console.log(req.body);
-		res.status(200).send('ok');
+
+		connection.query('SELECT * FROM items WHERE id="'+parseInt(req.body.item_id)+'"', function (error, result, fields) {
+			if (error) {
+				console.log('[Mysql] Error on load item.');
+				res.response(401).send('error al cargar el checkout.');
+
+				return;
+			}
+			
+			result = result[0];
+			
+			// Crea un objeto de preferencia
+			// NO obtener el precio del producto desde lo que pasas por medio de res/req por que puede ser modificado por el 
+			// usuario final.
+			let preference = {
+				items: [
+					{
+						title: result.name,
+						unit_price: parseFloat(result.price),
+						quantity: 1,
+					}
+				],
+				redirect_urls: {
+					failure: process.env.MERCADOPAGO_REDIRECT_URL,
+					pending: process.env.MERCADOPAGO_REDIRECT_URL,
+					success: process.env.MERCADOPAGO_REDIRECT_URL
+				},
+			};
+		
+			mercadopago.preferences.create(preference)
+			.then(function (response) {
+				console.log(response.body.id);
+				
+				connection.query("INSERT INTO payment (name, email, payment_id, status) VALUES (?,?,?,'PENDING')",
+				[req.body.name, req.body.email, response.body.id],
+				function(error, results, fields) {
+					if(error) {
+						console.log(error);
+						res.status(401).send('error');
+						
+						return;
+					}
+
+					res.redirect(response.body.init_point);
+				});
+			}).catch(function (error) {
+				console.log(error);
+				
+				res.status(401).send('error');
+			});
+		});
 	});
-	//app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); 
 	
-	const httpsServer = https.createServer({key, cert}, app);
-	httpsServer.listen(port, host, ()=> console.log(`Server Online port ${host}:${port}`));
-//}).catch( (err)=> console.log(err) );
+	app.post('/pagado', (req, res) => {
+		console.log('pagado exitoso');
+	});
+
+	app.get('*', (req, res) => { 
+		res.sendFile(path.join(__dirname, '../public/index.html'));
+	});
+	
+	const httpsServer = https.createServer(/*{key, cert}, */app);
+	httpsServer.listen(process.env.SERVER_PORT, process.env.SERVER_HOST, ()=> console.log(`Server on-line on ${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`));
+}).catch( (err)=> console.log(err) );
